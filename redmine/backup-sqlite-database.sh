0f01fad3842be1bf226b13bdc0be8758eee4be60
Tiago Katcipis <tiagokatcipis@gmail.com>
2013-08-17 15:26:09 -0300

    adding a new script to backup my personal redmine database

---
+++ redmine/backup-sqlite-database.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    echo "You must provide a user to own the backup !!!"
+    echo "Usage: "$0" <user>"
+    exit
+fi
+
+REDMINE_SQLITE_DATABASE="/var/lib/dbconfig-common/sqlite3/redmine"
+BACKUP_BASE_DIR="$HOME/Ubuntu One/redmine-backup"
+NOW=$(date +"%F")
+BACKUP_DIR=$BACKUP_BASE_DIR/$NOW/redmine
+
+echo "This script must be run as sudo, just trust me :-)"
+echo "It works for Redmine with sqlite under Apache"
+
+echo "Creating backup dir["$BACKUP_DIR"]"
+mkdir -p "$BACKUP_DIR"
+
+echo "Stopping apache to avoid redmine being used while we backup"
+service apache2 stop
+
+echo "Copying redmine database from["$REDMINE_SQLITE_DATABASE"] to ["$BACKUP_DIR"]"
+cp -pr "$REDMINE_SQLITE_DATABASE"/* "$BACKUP_DIR"
+
+echo "The backup will be owned by["$1"]"
+chown -R $1 "$BACKUP_BASE_DIR"
+
+echo "Starting apache back"
+service apache2 start
