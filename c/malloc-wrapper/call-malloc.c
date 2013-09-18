ec7b4549537d56a093e558dc88d934b95ae13e08
Tiago Katcipis <tiagokatcipis@gmail.com>
2013-09-18 20:17:33 -0300

    suck this manga :-(

--- c/malloc-wrapper/call-malloc.c
+++ c/malloc-wrapper/call-malloc.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "call-malloc.h"
 
 void call_malloc(void)
 {
+    printf("=== starting call_malloc on dynamic library ===\n");
     void * a = malloc(1024);
     void * b = malloc(2048);
     void * c = malloc(3000);
@@ -10,4 +12,5 @@ void call_malloc(void)
     free(a);
     free(b);
     free(c);
+    printf("=== call_malloc on dynamic library done ===\n\n");
 }
--- c/malloc-wrapper/malloc-wrapper.c
+++ c/malloc-wrapper/malloc-wrapper.c
@@ -4,7 +4,7 @@
 
 extern void * __real_malloc(size_t size);
 
-void * __wrapper_malloc(size_t size)
+void * __wrap_malloc(size_t size)
 {
     printf("wrapped malloc call for size[%zu]\n", size);
     void * ret = __real_malloc(size);
@@ -15,5 +15,9 @@ void * __wrapper_malloc(size_t size)
 int main(void)
 {
     call_malloc();
+    printf("=== calling malloc inside the bynary ===\n");
+    void * ptr = malloc(7777);
+    printf("=== done calling malloc inside the bynary ===\n");
+    free(ptr);
     return 0;
 }
