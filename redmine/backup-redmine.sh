c1945ffb58aec689152bcbf56ffb9e08b2b1f2ab
Tiago Katcipis <tiagokatcipis@gmail.com>
2013-11-24 14:30:38 -0200

    -adding the machine information.

--- redmine/backup-redmine.sh
+++ redmine/backup-redmine.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#Im using vagrant to build a redmine server, it is not usually what it is used for
+#But it is so easy to build a virtualbox machine on it that im using it anyway :-)
+#Usually it should be used to build dev environments, and for these cases doing a backup
+#is useless since you dont store anything crucial on a dev environment 
+#(provisioning should do the trick on this case).
+#The machine is a precise32: http://files.vagrantup.com/precise32.box 
+
 VAGRANT_VIRTUALBOX_MACHINE=$HOME/.vagrant.d/boxes/precise32/virtualbox
 VAGRANT_HOME_DIR=$HOME/workspace/works/redmine/vagrant
 BACKUP_DIR="$HOME/Ubuntu One/redmine-backup"
