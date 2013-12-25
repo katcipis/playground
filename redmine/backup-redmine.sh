259a156a39333510aa182cdb6eeafe296b7bbc45
Tiago Katcipis <tiagokatcipis@gmail.com>
2013-12-25 19:21:47 -0200

    -fixing my backup script

--- redmine/backup-redmine.sh
+++ redmine/backup-redmine.sh
@@ -7,15 +7,13 @@
 #(provisioning should do the trick on this case).
 #The machine is a precise32: http://files.vagrantup.com/precise32.box 
 
-VAGRANT_VIRTUALBOX_MACHINE=$HOME/.vagrant.d/boxes/precise32/virtualbox
 VAGRANT_HOME_DIR=$HOME/workspace/works/redmine/vagrant
+VAGRANT_BOX=$VAGRANT_HOME_DIR/package.box
 BACKUP_DIR="$HOME/Ubuntu One/redmine-backup"
 
-echo "Halting Vagrant machine"
+echo "Boxing Vagrant machine"
 cd $VAGRANT_HOME_DIR
-vagrant halt
-echo "Copying $VAGRANT_VIRTUALBOX_MACHINE to $BACKUP_DIR"
-cp -pr "$VAGRANT_VIRTUALBOX_MACHINE" "$BACKUP_DIR"
-echo "running Vagrant again"
-vagrant up
+vagrant package
+echo "Moving $VAGRANT_BOX to $BACKUP_DIR"
+mv "$VAGRANT_BOX" "$BACKUP_DIR"
 echo "done"
