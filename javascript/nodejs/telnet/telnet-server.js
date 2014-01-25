9a2d405cf2d98658680e222b3f0677e4c3646aa9
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-01-25 14:47:58 -0200

    -starting a new node example, implementing a telnet server. Not working yet because i have to remove the newline from the received command.

---
+++ javascript/nodejs/telnet/telnet-server.js
@@ -0,0 +1,53 @@
+const
+port = 5432,
+     fs = require('fs'),
+     net = require('net'),
+     filename = process.argv[2],
+     spawn = require('child_process').spawn,
+     server = net.createServer(function(connection) {
+         console.log('Subscriber connected.');
+
+         connection.on('data', function(data) {
+             console.log('received command: ' + data);
+             var parsed_cmd = data.toString().split(" ");
+             console.log('spawning command');
+             var cmd_name = parsed_cmd[0];
+             var cmd_args = parsed_cmd.slice(1);
+             console.log('name: ' + cmd_name + ' args: [' + cmd_args + ']');
+             //var cmd = spawn('ls', ['-lh']);
+             var cmd = spawn(cmd_name, cmd_args);
+
+             cmd.on('error', function (err) {
+                 connection.write('Error executing command: ' + err);
+                 connection.write('\n\n');
+             });
+
+             cmd.on('exit', function () {
+                 console.log('command finished');
+                 connection.write('\n\n');
+             });
+
+             cmd.stdout.setEncoding('utf8');
+             cmd.stdout.on('data', function (stdout_data) {
+                 console.log('sending process stdout to client');
+                 connection.write(stdout_data);
+             });
+             cmd.stdout.on('end', function () {
+                 console.log('command will provide no more data');
+             });
+             cmd.stderr.setEncoding('utf8');
+             cmd.stderr.on('data', function (err) {
+                 console.log('writing process stderr to client');
+                 connection.write('Error executing command: ' + err);
+             });
+         });
+
+         connection.on('close', function() {
+             console.log('Subscriber disconnected.');
+         });
+     });
+
+server.listen(port, function() {
+    console.log('Listening for telnet access on port: ' + port);
+    console.log('run: telnet <ip> ' + port + ' to access');
+});
