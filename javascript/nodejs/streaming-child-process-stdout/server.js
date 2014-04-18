753c209b6118c8621730d47d38cb22f3c96b444d
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-04-18 14:14:53 -0300

    -finally i can reproduce a leak :-)

--- javascript/nodejs/streaming-child-process-stdout/server.js
+++ javascript/nodejs/streaming-child-process-stdout/server.js
@@ -7,34 +7,18 @@ app.get('/',  function(req, res) {
     var headers = {
         'Content-Type': 'application/octet-stream'
     }
-    var args = [
-        '--protocol', 'ssh', 
-        '--address', '',
-        '--file-location', '',
-        '--login' , '',
-        '--password', ''
-    ];
-    var streamerProcess = childProcess.spawn('./streamer', args);
+    var streamerProcess = childProcess.spawn('./streamer');
+    streamerProcess.stdout.on('data', function(data) {
+        res.write(data);
+    });
 
-    var parseMediaInformation = function(data) {
-        streamerProcess.stdout.removeListener('data', parseMediaInformation);
-        var info = JSON.parse(data);
-        var mediaLength = info.len;
-        var sendMedia = function(data) {
-            if (data.length > mediaLength) {
-                data = data.slice(0, mediaLength);
-            }
-            mediaLength -= data.length;
-            res.write(data);
-            if (mediaLength === 0) {
-                res.end();
-            }
-        };
-        streamerProcess.stdout.on('data', sendMedia);
-        streamerProcess.stdin.write('\n');
-    };
+    streamerProcess.on('exit', function() {
+        res.end();
+        streamerProcess.removeAllListeners();
+        streamerProcess.stderr.removeAllListeners();
+        streamerProcess.stdout.removeAllListeners();
+    });
 
-    streamerProcess.stdout.on('data', parseMediaInformation);
     res.writeHeader(200, headers);
 });
 
