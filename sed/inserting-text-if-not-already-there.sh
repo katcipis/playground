8680dd22c455bfd3d67b55593a353726bb68f2d7
Tiago Katcipis <tiago.katcipis@gmail.com>
2014-06-03 20:02:03 -0300

    -fixing a comment.

--- sed/inserting-text-if-not-already-there.sh
+++ sed/inserting-text-if-not-already-there.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 INSERTED_TEXT="set tags+=~/path/to/ctags/sed/example"
-#lets insert the damn escape characters, delete command doesnt allow changing the "/" delimiter
+#lets insert the damn escape characters, delete command doesn't allow changing the "/" delimiter
 #`command` and $(command) does not behave the same way, DAMN YOU SHELL !!!
 ESCAPED_INSERTED_TEXT=$(echo $INSERTED_TEXT | sed 's:/:\\/:g')
 FILE="example.txt"
