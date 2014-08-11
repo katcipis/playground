52c2b624108e7fa6ee885e316ba619c891345241
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-08-10 22:32:41 -0300

    -first version on the amazing transcoder with subtitles :P

--- c/gstreamer/video-conversion/1.0/to-h264-aac-mp4.sh
+++ c/gstreamer/video-conversion/1.0/to-h264-aac-mp4.sh
@@ -1,6 +1,8 @@
 echo ""
-echo "GStreamer 1.0 video transcoder."
-echo "usage: $0 <filename>"
+echo "GStreamer 1.0 video transcoder. It will transcode videos to a more usual TV friendly format and also overlay subtitles."
+echo "usage: $0 <video filepath> <subtitle (srt) filepath>"
 echo ""
 
-gst-launch-1.0 filesrc location=$1 ! decodebin name=decoder ! queue ! videoconvert ! x264enc ! mp4mux name=muxer ! filesink location=$1-converted.mp4 decoder. ! queue ! voaacenc ! muxer.
+echo "Video filepath: "$1" Srt filepath: "$2
+
+gst-launch-1.0 filesrc location=$1 ! decodebin name=decoder ! queue ! videoconvert ! subtitleoverlay name=subtitle ! x264enc ! mp4mux name=muxer ! filesink location=$1-converted.mp4 decoder. ! queue ! voaacenc ! muxer. filesrc location=$2 ! queue ! subtitle.subtitle_sink
