1c6a20697a9c70b6b2702462cd382a4ffccbfa3f
Tiago Cesar Katcipis <tiago.katcipis@digitro.com.br>
2014-08-27 17:10:04 -0300

    yeah...it is pretty clear now...i am idiot :D

--- javascript/nodejs/writing-on-closed-response/writing-on-closed-response.js
+++ javascript/nodejs/writing-on-closed-response/writing-on-closed-response.js
@@ -8,10 +8,10 @@ var app = express();
 app.get('/',  function(req, res) {
     res.setHeader('Content-Type', 'application/octet-stream');
     res.on("close", function() {
-        console.log("RESPONSE CLOSED BY THE CLIENT !!!!");
+        console.log("SERVER: RESPONSE CLOSED BY THE CLIENT !!!!");
     });
     setInterval(function() {
-        console.log("writing data on the response");
+        console.log("SERVER: writing data on the response");
         res.write(new Buffer(64));
     }, 1000);
 });
@@ -20,6 +20,7 @@ var server = http.createServer(app);
 server.listen(8080);
 
 var clientReq = http.get("http://localhost:8080", function(res) {
-    console.log("Got response: " + res.statusCode);
+    console.log("CLIENT: Got response: " + res.statusCode);
+    console.log("CLIENT: Aborting the request to close the connection");
     clientReq.abort();
 });
