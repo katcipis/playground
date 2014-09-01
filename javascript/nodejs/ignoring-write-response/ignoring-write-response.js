416e5ebf9a726b2ba6eb625533af7fad9cbe468c
Tiago Cesar Katcipis <tiago.katcipis@digitro.com.br>
2014-09-01 11:36:57 -0300

    adding a new test to check why memory usage is getting so high

---
+++ javascript/nodejs/ignoring-write-response/ignoring-write-response.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.setHeader('Content-Type', 'application/octet-stream');
+    setInterval(function() {
+        console.log("SERVER: writing data on the response");
+        console.log("SERVER: write response: " + res.write(new Buffer(60000)));
+    }, 50);
+});
+
+server.listen(8080);
+
+var clientReq = http.get("http://localhost:8080", function(res) {
+    console.log("CLIENT: Got response: " + res.statusCode);
+    res.on('data', function() {
+        console.log("CLIENT: got data");
+    });
+    res.on('end', function() {
+        console.log("CLIENT: response ended");
+    });
+});
