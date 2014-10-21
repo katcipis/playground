6c812fe4b9ee203b1c991a2f76bb71f53f8ff537
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-10-20 23:46:57 -0200

    adding an example of live audio streamer.

---
+++ javascript/nodejs/live-audio-streaming/live-audio.js
@@ -0,0 +1,22 @@
+var http = require("http");
+var fs = require("fs");
+var childProcess = require("child_process");
+
+var server = http.createServer(function(req, res) {
+
+    if (req.url === "/live-audio") {
+        var liveStreamer = childProcess.spawn("./streamer.sh");
+        res.setHeader("Content-Type", "audio/mp3");
+        liveStreamer.stdout.pipe(res);
+        liveStreamer.stderr.setEncoding('utf-8');
+        liveStreamer.stderr.on('data', function(data) {
+            console.warn(data);
+        });
+        return;
+    }
+
+    res.write(fs.readFileSync("./index.html"));
+    res.end();
+});
+
+server.listen(8081);
