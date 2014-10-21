613eb4f5cb085ad68d3d129398b57f5029b9a614
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-10-21 00:03:55 -0200

    lets try a little more with ogg.

--- javascript/nodejs/live-audio-streaming/live-audio.js
+++ javascript/nodejs/live-audio-streaming/live-audio.js
@@ -5,8 +5,8 @@ var childProcess = require("child_process");
 var server = http.createServer(function(req, res) {
 
     if (req.url === "/live-audio") {
-        var liveStreamer = childProcess.spawn("./mp3-streamer.sh");
-        res.setHeader("Content-Type", "audio/mp3");
+        var liveStreamer = childProcess.spawn("./ogg-streamer.sh");
+        res.setHeader("Content-Type", "application/ogg");
         liveStreamer.stdout.pipe(res);
         liveStreamer.stderr.setEncoding('utf-8');
         liveStreamer.stderr.on('data', function(data) {
