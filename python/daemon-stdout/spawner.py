dd984931b14bcf36b0a37b2f49b7cf4dd01513ad
Tiago <tiagokatcipis@gmail.com>
2015-05-04 21:33:08 -0300

    adding some more details

--- python/daemon-stdout/spawner.py
+++ python/daemon-stdout/spawner.py
@@ -9,9 +9,14 @@ print("Starting")
 daemon = subprocess.Popen(["python", "daemon.py"], stdout=_stdout, stderr=_stderr)
 
 time.sleep(5)
+
+print("File contents before terminate:")
+print("STDOUT: " + _stdout.read())
+print("STDERR: " + _stderr.read())
 print("Terminating daemon")
 daemon.terminate()
 daemon.wait()
 
+print("\nFile contents after terminate:")
 print("STDOUT: " + _stdout.read())
 print("STDERR: " + _stderr.read())
