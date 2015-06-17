f8c013bfb370d824a9b432172d6f53b17eed4374
Tiago <tiagokatcipis@gmail.com>
2015-06-17 18:01:34 -0300

    testing for exceptions is pretty easy

--- clojure/calc/src/calc/core.clj
+++ clojure/calc/src/calc/core.clj
@@ -1,9 +1,16 @@
 (ns calc.core
   (:gen-class))
 
+;Variadic stuff: http://clojure-doc.org/articles/language/functions.html#variadic-functions
 (defn add 
   ([& args]
   (apply + args)))
+
+;Pre conditions
+(defn divide 
+  ([a b]
+   {:pre [(> b 0)]}
+  (/ a b)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- clojure/calc/test/calc/core_test.clj
+++ clojure/calc/test/calc/core_test.clj
@@ -2,14 +2,16 @@
   (:require [clojure.test :refer :all]
             [calc.core :refer :all]))
 
-(deftest a-test
+(deftest adding-numbers
   (testing "Adding two numbers"
-    (is (= 2 (calc.core/add 1 1)))))
-
-(deftest a-test
+    (is (= 2 (calc.core/add 1 1))))
   (testing "Adding three numbers"
-    (is (= 5 (calc.core/add 1 1 3)))))
-
-(deftest a-test
+    (is (= 5 (calc.core/add 1 1 3))))
   (testing "Adding five numbers"
     (is (= 17 (calc.core/add 1 1 3 5 7)))))
+
+(deftest dividing-numbers
+  (testing "dividing two numbers"
+    (is (= 2 (calc.core/divide 4 2))))
+  (testing "cant divide by zero"
+    (is (thrown? AssertionError (calc.core/divide 4 0)))))
