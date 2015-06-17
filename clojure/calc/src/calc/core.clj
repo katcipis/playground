a916a6e02270834e7c35d81c4f1d2151dfe8c06f
Tiago <tiagokatcipis@gmail.com>
2015-06-17 16:24:32 -0300

    using variadic args

--- clojure/calc/src/calc/core.clj
+++ clojure/calc/src/calc/core.clj
@@ -2,10 +2,8 @@
   (:gen-class))
 
 (defn add 
-  ([a b]
-  (+ a b))
-  ([a b c]
-  (+ a b c)))
+  ([& args]
+  (apply + args)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- clojure/calc/test/calc/core_test.clj
+++ clojure/calc/test/calc/core_test.clj
@@ -9,3 +9,7 @@
 (deftest a-test
   (testing "Adding three numbers"
     (is (= 5 (calc.core/add 1 1 3)))))
+
+(deftest a-test
+  (testing "Adding five numbers"
+    (is (= 17 (calc.core/add 1 1 3 5 7)))))
