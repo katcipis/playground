1da950e1a6d634034817e972f33aefd0a6e9d8d4
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-08-02 20:54:01 -0300

    Improve code

--- go/string/main.go
+++ go/string/main.go
@@ -5,21 +5,45 @@ import "fmt"
 func main() {
 	samplestr := "canção"
 
-	fmt.Println("the idea of this code is to show that string behavior")
-	fmt.Println("in go is not consistent and that implementation details leaks")
-	fmt.Println("strings are actually byte slices and it behaves as one")
+	fmt.Println(`
+The idea of this code is to show that string behavior
+in go is not consistent and that implementation details leaks.
+strings are actually byte slices and they behave as one to almost
+all operations, but not all, hence inconsistent.
+`)
 
 	fmt.Printf("string[%s]: len[%d] type[%T]\n", samplestr, len(samplestr), samplestr)
-	fmt.Printf("string value being accessed through index: value[%d] type[%T]\n", samplestr[0], samplestr[0])
 
-	fmt.Println("iterating a string won't return you its bytes, but runes")
-	iterationsCount := 0
+	fmt.Println(`
+The length of a string behaves as a byte slice, giving a size in bytes.
+Iterating the string with a numeric for will also get you byte slice behavior:
+`)
+	bytesCount := 0
+	for i := 0; i < len(samplestr); i++ {
+		b := samplestr[i]
+		fmt.Printf("index[%d] value[%v] type[%T]\n", i, b, b)
+		bytesCount += 1
+	}
+	fmt.Printf("iterations through len: %d\n", bytesCount)
+
+	fmt.Println(`
+But iterating the string with range will get you rune slice behavior.
+This is so inconsistent that you will see something very odd happening, the index
+on the for loop will jump in steps of two:
+`)
+	runesCount := 0
 	for i, r := range samplestr {
 		fmt.Printf("index[%d] value[%v] type[%T]\n", i, r, r)
-		iterationsCount += 1
+		runesCount += 1
 	}
+	fmt.Printf("iterations through range: %d\n", runesCount)
 
-	fmt.Printf("the len of the string is [%d] but the amount of iterations looping through it is [%d]\n", len(samplestr), iterationsCount)
-	fmt.Println("quite inconsistent and showcases that it is not safe to use len or access a string through slice operations")
-	fmt.Println("a string is a set of 'something' that has different cardinality depending on how you iterate through it")
+	fmt.Println(`
+summing up: len and index operations behave as byte slice")
+range iteration behaves as rune slice
+quite inconsistent and showcases that it is not safe to use
+len or access a string through indexing (it will only work for ASCII)
+a string is a set of 'something' that has different
+cardinality and values depending on how you iterate or access it
+`)
 }
