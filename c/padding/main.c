d7a127a870ad9aae5da4a5818b93a92c82c8e94d
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-01-27 21:25:35 +0100

    Add padding fun

--- c/padding/main.c
+++ c/padding/main.c
@@ -7,9 +7,8 @@ typedef struct{
 } no_padding;
 
 typedef struct{
-    char a;
-    short b;
-    char c;
+    int a;
+    char b;
 } padding_array;
 
 int main(int argc, char **argv) {
@@ -18,7 +17,7 @@ int main(int argc, char **argv) {
     printf("no_padding: address: %p\n", &two_no_padding);
     printf("no_padding: size array: %i\n", sizeof(two_no_padding));
 
-    printf("sizeof short: %i\n", sizeof(short));
+    printf("sizeof int: %i\n", sizeof(int));
     printf("padding_array: size type: %i\n", sizeof(padding_array));
     padding_array two_padding_array[2];
     printf("padding_array: address: %p\n", &two_padding_array);
