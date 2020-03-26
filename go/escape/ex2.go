fad4e9d78292a7b8f9c5a26e696b672cc4a89bf7
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-03-26 12:35:04 +0100

    Add some escape analysis examples

---
+++ go/escape/ex2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func lala() int {
+	var a int
+	var b int
+	var c int
+
+	return a + b + c
+}
+
+func main() {
+	fmt.Println(lala())
+}
---
+++ go/escape/ex3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func lala() int {
+	var a int
+	var b int
+	var c int
+
+	fmt.Println(b)
+	return a + b + c
+}
+
+func main() {
+	fmt.Println(lala())
+}
