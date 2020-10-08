2e1f2d1f4d1ebb844acbc8f56555480afb3ae4a8
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-10-08 23:06:23 +0200

    Add logs links

--- python/wtf/logs.py
+++ python/wtf/logs.py
@@ -6,3 +6,7 @@ log.setLevel(logging.DEBUG)
 log.debug("debug")
 log.info("info")
 log.warning("warning")
+
+# Wont work because...why simple if it can be complicated ?
+# https://docs.python.org/3/howto/logging.html#logging-advanced-tutorial
+# https://stackoverflow.com/questions/57742985/setting-log-level-to-logging-debug-or-logging-info-has-no-effect
