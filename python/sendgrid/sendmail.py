00015e49af2b6e2050717bb8f4a9d8a871a3851d
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-02-02 11:18:08 +0100

    Send contents from file :D

--- python/sendgrid/sendmail.py
+++ python/sendgrid/sendmail.py
@@ -6,12 +6,16 @@ from sendgrid.helpers.mail import Mail
 
 from_email=sys.argv[1]
 to_email=sys.argv[2]
+contents_path=sys.argv[3]
+
+with open(contents_path) as f:
+    contents = f.read()
 
 message = Mail(
     from_email=from_email,
     to_emails=to_email,
     subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>Testing email integration with SendGrid :D</strong>')
+    html_content=contents)
 
 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
 response = sg.send(message)
