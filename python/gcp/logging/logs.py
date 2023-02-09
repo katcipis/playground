96973bbb5eaa7e252a0e78bd8cabefb6b6641aa5
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-02-10 00:28:54 +0100

    feat: add opensearch example

--- python/gcp/logging/logs.py
+++ python/gcp/logging/logs.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 from google.cloud.logging.handlers import StructuredLogHandler
 
 handler = StructuredLogHandler()
---
+++ python/opensearch/main.py
@@ -0,0 +1,22 @@
+import os
+from opensearchpy import OpenSearch
+
+client = OpenSearch(
+    hosts=os.getenv("ELASTICSEARCH_IP"),
+    port=os.getenv("ELASTICSEARCH_PORT"),
+    http_auth=(os.getenv("ELASTICSEARCH_USER"), os.getenv("ELASTICSEARCH_PASSWORD")),
+    timeout=30,
+    use_ssl=True,
+)
+
+index_name = 'my-dsl-index'
+index_body = {
+  'settings': {
+    'index': {
+      'number_of_shards': 4
+    }
+  }
+}
+
+response = client.indices.create(index_name, body=index_body)
+print(response)
