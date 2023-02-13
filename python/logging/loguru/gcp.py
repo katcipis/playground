96827f21c70dae4d8d1359b2f683ed967ae927cf
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-02-13 20:00:06 +0100

    chore: add gcp example

---
+++ python/logging/loguru/gcp.py
@@ -0,0 +1,13 @@
+from loguru import logger
+from google.cloud.logging.handlers import StructuredLogHandler
+
+handler = StructuredLogHandler()
+logger.remove()
+logger.add(handler, level="INFO", serialize=True, enqueue=False)
+context_logger = logger.bind(ip="192.168.0.1", user="someone")
+
+
+if __name__ == "__main__":
+    for _ in range(10):
+        context_logger.info("Contextualize your logger easily")
+        context_logger.error("ERROR")
