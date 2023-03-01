f669dfb019a0b07b8b80f6dc93eff77ada73c72c
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-03-01 21:12:30 +0100

    chore: add level changing

--- python/gcp/logging/logs.py
+++ python/gcp/logging/logs.py
@@ -4,10 +4,16 @@ from google.cloud.logging.handlers import StructuredLogHandler
 handler = StructuredLogHandler()
 logging.getLogger().addHandler(handler)
 
+
+logging.getLogger().setLevel("DEBUG")
+
 logging.debug("hi")
 logging.info("hi")
 logging.warning("hi")
 logging.error("hi")
+
+
+logging.getLogger().setLevel("INFO")
 
 child = logging.getLogger("module")
 child.debug("hi")
