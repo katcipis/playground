044b1acb627fef99cd03af54e7111c6788038fde
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-03-05 02:25:34 +0100

    chore: finish heron algorithm to calculate sqrt in Rust

--- rust/heron/src/lib.rs
+++ rust/heron/src/lib.rs
@@ -1,30 +1,38 @@
 // Inspired from SICP lectures :-)
+// This is very closure heavy and inneficient, but it is a good exercise to:
+// - understand the overall algorithm (guess + avg dampener + fixed point searching)
+// - check how dynamic closures are used/defined in Rust
 
 pub fn sqrt(x: f64) -> f64 {
-    let dampener = avg_damp(move |y| x / y);
-    let find_fixed_point = fixed_point(dampener);
-    let guess = 1.0;
+    let dampened_guesser = avg_damp(move |y| x / y);
+    let find_fixed_point = fixed_point(dampened_guesser);
+    let initial_guess = 1.0;
 
-    find_fixed_point(guess)
+    find_fixed_point(initial_guess)
 }
 
-// Why returning the close is different than taking it as parameter ?
+// Why returning the closure is different than taking it as parameter ?
 // - https://doc.rust-lang.org/rust-by-example/fn/closures/input_parameters.html
 // - https://doc.rust-lang.org/book/ch19-05-advanced-functions-and-closures.html#returning-closures
 // - https://doc.rust-lang.org/reference/types/closure.html
 
-fn fixed_point<F>(f: F) -> Box<dyn Fn(f64) -> f64>
-where
-    F: Fn(f64) -> f64,
-{
-    Box::new(|x| x)
+fn fixed_point<F: Fn(f64) -> f64 + 'static>(f: F) -> Box<dyn Fn(f64) -> f64> {
+    Box::new(move |x| {
+        let mut x = x;
+        for _ in 1..10000 {
+            let next = f(x);
+            if next == x {
+                return x;
+            }
+            x = next;
+        }
+        // Ugly hack =P
+        panic!("fixed point failed to converge")
+    })
 }
 
-fn avg_damp<F>(f: F) -> Box<dyn Fn(f64) -> f64>
-where
-    F: Fn(f64) -> f64 + 'static,
-{
-    Box::new(move |x| f(x) + x / 2.0)
+fn avg_damp<F: Fn(f64) -> f64 + 'static>(f: F) -> Box<dyn Fn(f64) -> f64> {
+    Box::new(move |x| (f(x) + x) / 2.0)
 }
 
 #[cfg(test)]
@@ -33,7 +41,10 @@ mod test {
 
     #[test]
     fn test_sqrt() {
+        assert_eq!(sqrt(1.0), 1.0);
         assert_eq!(sqrt(4.0), 2.0);
+        assert_eq!(sqrt(9.0), 3.0);
         assert_eq!(sqrt(16.0), 4.0);
+        assert_eq!(sqrt(25.0), 5.0);
     }
 }
